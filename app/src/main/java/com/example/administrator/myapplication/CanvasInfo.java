01b926e8fae229a02aafa70cee514203e1159131
vjiang <v-vjiang@expedia.com>
2017-05-03 18:00:55 +0800

    oop

---
+++ app/src/main/java/com/example/administrator/myapplication/CanvasInfo.java
@@ -0,0 +1,8 @@
+package com.example.administrator.myapplication;
+
+/**
+ * Created by Administrator on 2017/5/3.
+ */
+
+public class CanvasInfo {
+}
---
+++ app/src/main/java/com/example/administrator/myapplication/LineInfo.java
@@ -0,0 +1,9 @@
+package com.example.administrator.myapplication;
+
+/**
+ * Created by Administrator on 2017/5/3.
+ */
+
+public class LineInfo {
+
+}
